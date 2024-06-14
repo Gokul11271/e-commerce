527801c8f49b57c4ab8328fa49808d4c3eebf48f
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2024-06-14 09:56:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# e-commerce site
+ my sample e-commerce site
---
+++ index.js
@@ -0,0 +1,9 @@
+var sidenav = document.querySelector(".side-navbar");
+
+function showSideNavbar() {
+    sidenav.style.display = "block";
+}
+
+function closeNavbar() {
+    sidenav.style.display = "none";
+}
